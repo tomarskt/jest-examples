7ca476667f8c76c5d63a99a580c5e7f6c4bd7901
Bo Chen <bo.chen@kindredplc.com>
2017-03-01 16:58:32 +1100

    update

--- enzyme/legacy/__tests__/CheckboxWithLabel-test.js
+++ enzyme/legacy/__tests__/CheckboxWithLabel-test.js
@@ -15,9 +15,13 @@ describe('shallow', () => {
 
     //click button defined in child component
     //const myBtn = wrapper.find('button')   // this will fail the test as we are just 'shallow' rendering the ToggleCheckbox 
-    const myBtn = wrapper.find('MyButton')
-    expect(myBtn.node).toBeTruthy()
+    const myBtn = wrapper.find('MyButton') //we only render the fist level components
+    expect(myBtn.node).toBeTruthy() //this is react component defined by `import MyButton from './mybutton'`
     myBtn.simulate('click'); // MyButton.onClick won't invoked; it's generally a bad idea to fire up events in shallow rendering case
+                             // AND **  my_callback in <MyButton onClick={ my_callback } /> is blindly called  **
+                             // I have a code inside MyButton which comment out the line of `this.props.onClick()` and that will only impact `mount`
+                             // becuase in `shallow` case, callbacks are just **blindly** called
+                             // the other difference between `shallow` and `mount` is that React lifecycle events like DidMount won't fired for `shallow` (just like server side rendering)
 
     //check it again
     checkbox = wrapper.find('label')
