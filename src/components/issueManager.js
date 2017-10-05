d8079699d82dd33f0eef5db65c5db81f90c1b6c9
Bo Chen <bochen2014@yahoo.com>
2017-10-06 01:31:00 +1100

    finalize solution

--- src/components/issueManager.js
+++ src/components/issueManager.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { sortIssueByTimeStr, filterIssueByState, normalize, fetch } from '../common';
+import IssueFilter from './issueFilter';
 import IssueItem from './issueItem';
 
 class IssueManager extends Component {
@@ -14,34 +15,38 @@ class IssueManager extends Component {
         }
     }
     componentDidMount() {
+        this.updateRepo();
+    }
+
+    onRepoChanged = e => {
+        const { target: { value } } = e;
+        const { fetchUrl } = this.state;
+        if (value !== fetchUrl) {
+            this.setState({
+                fetchUrl: value,
+            })
+        }
+    }
+
+    updateRepo = e => {
         const { fetchUrl, sortOrder } = this.state;
         this.setState({
             loading: true,
+            selectedFilter: 'all',
+            sortOrder: 'descending',
         });
 
-        // fetch(fetchUrl)
-        //     .then(res => {
-        //         const issues = normalize(res).sort(sortIssueByTimeStr(sortOrder));
-        //         this.setState({
-        //             loading: false,
-        //             issues,
-        //         })
-        //     })
-        //     .catch(err => {
-        //         console.error(err);
-        //     })
-
-
-        const self = this;
-        setTimeout(function () {
-            const issues = [{ "id": 263082153, "number": 11108, "state": "open", "title": "Clear previous children when SVG node doesn't have innerHTML", "user": { "id": 2384068, "avatarUrl": "https://avatars3.githubusercontent.com/u/2384068?v=4", "name": "OriR" }, "dateCreated_raw": "2017-10-05T10:55:42Z", "dateCraetedStr": "14 minutes ago", "tags": ["CLA Signed"] }, { "id": 263081500, "number": 11107, "state": "open", "title": "Document that deep equality checks and JSON.stringify() in shouldComponentUpdate() are a bad idea", "user": { "id": 810438, "avatarUrl": "https://avatars0.githubusercontent.com/u/810438?v=4", "name": "gaearon" }, "dateCreated_raw": "2017-10-05T10:53:09Z", "dateCraetedStr": "17 minutes ago", "tags": ["Component: Documentation & Website"] }, { "id": 263051011, "number": 11106, "state": "closed", "title": "Asynchronous ComponentWillUnmount in React 16", "user": { "id": 8241115, "avatarUrl": "https://avatars0.githubusercontent.com/u/8241115?v=4", "name": "aj-anuragjain" }, "dateCreated_raw": "2017-10-05T09:01:18Z", "dateCraetedStr": "2 hours ago", "tags": ["Component: Core", "Type: Question"] }]
-            self.setState({
-                loading: false,
-                issues,
-            });
-
-        }, 100);
-
+        fetch(fetchUrl)
+            .then(res => {
+                const issues = normalize(res).sort(sortIssueByTimeStr(sortOrder));
+                this.setState({
+                    loading: false,
+                    issues,
+                })
+            })
+            .catch(err => {
+                console.error(err);
+            })
     }
 
     onSortOrderChanged = e => {
@@ -54,46 +59,35 @@ class IssueManager extends Component {
             issues: issuesNew
         })
     }
-    onFilterClick = newFilter => e => {
+
+    onFilterChanged = newFilter => e => {
         const { issues } = this.state;
         this.setState({
             selectedFilter: newFilter,
         })
     }
 
-
     render() {
-        const { issues, loading, selectedFilter, sortOrder } = this.state;
+        const { issues, fetchUrl, loading, selectedFilter, sortOrder } = this.state;
         let totalCount, openIssuesCount, closedIssuesCount, issues2Display;
         if (!loading) {
-            totalCount = issues.length;
-            openIssuesCount = issues.filter(i => i.state === 'open').length;
-            closedIssuesCount = issues.filter(i => i.state === 'closed').length;
             issues2Display = issues.filter(filterIssueByState(selectedFilter))
         }
         return <div>
-            <div>
-                <label htmlFor="" htmlFor="">repo url:</label><input type="text" /><button>Update</button></div>
-            <div>
+            <div id="change-repo">
+                <label htmlFor="repo-url">repo url:</label>
+                <input type="text" name="repo-url" style={{ width: 600 }}
+                    value={fetchUrl} onChange={this.onRepoChanged} />
+                <button onClick={this.updateRepo}>Update</button>
+            </div>
+            <div id="issue-container">
                 {loading ? <h2>loading issue list...</h2> :
                     <div className="issues-wrapper">
-                        <div className="issue-item issue-item-filter">
-                            <div className="filter-by">
-                                <span onClick={this.onFilterClick('all')} className={selectedFilter === 'all' ? 'active' : ''}>{totalCount} All</span>
-                                <span onClick={this.onFilterClick('open')} className={selectedFilter === 'open' ? 'active' : ''}>{openIssuesCount} Open</span>
-                                <span onClick={this.onFilterClick('closed')} className={selectedFilter === 'closed' ? 'active' : ''}>{closedIssuesCount}Closed</span>
-                            </div>
-                            <div className="sort-by">
-                                <label htmlFor="sel_sort_order">Sort By</label>
-                                <select disabled={true}>
-                                    <option>created date</option>
-                                </select>
-                                <select name='sel_sort_order' value={sortOrder} onChange={this.onSortOrderChanged}>
-                                    <option value='descending'>latest first</option>
-                                    <option value='ascending'>oldest first</option>
-                                </select>
-                            </div>
-                        </div>
+                        <IssueFilter issues={issues}
+                            selectedFilter={selectedFilter}
+                            sortOrder={sortOrder}
+                            onFilterChanged={this.onFilterChanged}
+                            onSortOrderChanged={this.onSortOrderChanged} />
                         <ul>
                             {issues2Display.map(i => <IssueItem key={`issue-${i.id}}`} issue={i} />)}
                         </ul>
