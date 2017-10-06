b6d7986e0f79a3dbfbe18b77244e090d62d72b7f
Bo Chen <bochen2014@yahoo.com>
2017-10-06 22:44:44 +1100

    polyfill module system for typescript

--- typescript/FKEventHeaderTemplate.tsx
+++ typescript/FKEventHeaderTemplate.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'; // for typescript to work, module must have module.exports.default={xyz}
 import RaceTimeStatus from 'Frankel/components/RaceTimeStatus'
 
 export function FKEventHeaderTemplate(props) {
