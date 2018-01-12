52d00512e8d5f61b1cbba10cac44a7cfd0aa5766
Bo Chen <bo.chen@vodafone.com.au>
2018-01-12 12:47:45 +1100

    update

--- timer/lodash.spec.js
+++ timer/lodash.spec.js
@@ -10,7 +10,9 @@ test('fails to mock Lodash timers correctly', function() {
   debounced();
 
   // jest.runAllTimers(); // because there is nested settimeout (i.e. calling setTimeout within setTimeout), this will be an infinite loop;
-  jest.runOnlyPendingTimers();
+  jest.runOnlyPendingTimers(); // https://github.com/facebook/jest/issues/3465#issuecomment-341717467
+
+  // fix:  https://github.com/facebook/jest/issues/3465#issuecomment-351186130
 
   expect(test).toHaveBeenCalledTimes(1);
 });
