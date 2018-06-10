772036bdccdd0c714ce44db0784640a5a11a171a
Bo Chen <bochen2014@yahoo.com>
2018-06-10 16:56:19 +1000

    jest.mock(../MyButton, ()=>"my-fancy-button")

--- enzyme/components/__tests__/ToggleCheckbox.test-render.js
+++ enzyme/components/__tests__/ToggleCheckbox.test-render.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
+
+// https://facebook.github.io/jest/docs/en/tutorial-react.html#snapshot-testing-with-mocks-enzyme-and-react-16
+// unlike 'enzyme', 'react-test-render' doesn't care about Upper case dom element names, and will happily accept e.g. <SomeComponent>.
+jest.mock('../Mybutton', ()=>'MyFancyButton')
+
 import ToggleCheckbox from '../ToggleCheckbox';
 
 
