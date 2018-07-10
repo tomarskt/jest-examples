390ddd1f036b24c6060077306486985a198880ee
Bo Chen <bo.chen@westpac.com.au>
2018-07-10 12:03:33 +1000

    add comments

--- enzyme/HOC/pageHeader.full.spec.jsx
+++ enzyme/HOC/pageHeader.full.spec.jsx
@@ -3,12 +3,12 @@
 // }));
 
 debugger;
-jest.mock('react-router');
+jest.mock('react-router'); //=================================> this is hoisted by jest, to make sure it's exuected before *anything else*
 const ReactRouter = require("react-router");
 
 
 console.warn(ReactRouter.withRouter);
-ReactRouter.withRouter.mockImplementation(()=>{
+ReactRouter.withRouter.mockImplementation(()=>{  //++++ this is NOT hoisted  +++ that's why this approach doesn't work 
     debugger;
     return 1;
 });
@@ -16,7 +16,7 @@ const val = ReactRouter.withRouter('hello');
 import React from "react";
 import renderer from "react-test-renderer";
 
-import PageHeader from "./pageHeader"; // ========================> this is hoisted; all import statement are hoisted, so it's executed before your jock statement;
+import PageHeader from "./pageHeader"; // ========================> this is hoisted by babel; all import statement are hoisted, so it's executed before your jock statement;
 
 describe("PageHeader Test", () => {
   it("should match snapshot for simple PageHeader", () => {
