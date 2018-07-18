081827345faf47b0cc6935bd31fc1e843edaa036
Bo Chen <bo.chen@westpac.com.au>
2018-07-18 15:13:11 +1000

    escape hatch: https://github.com/facebook/jest/issues/2567#issuecomment-272015844

--- enzyme/iframe/Sandbox.spec.js
+++ enzyme/iframe/Sandbox.spec.js
@@ -4,7 +4,12 @@ import { shallow, mount } from "enzyme";
 import Sandbox from "./Sandbox";
 const dispatchMocked = require("./store").dispatch; // jest.mock will be hoisted, so it's fine to do require before jest.mock;
 
-jest.mock("./displayloader", () => "MockDisplayLoader"); //defualt export mock
+//******************************************************************************************** */
+// babel-plugin-jest-hoist: The module factory of `jest.mock()` is not allowed to reference any out-of-scope variables.
+const mockEscapeHatch = 'MockDisplayLoader'; // https://github.com/facebook/jest/issues/2567#issuecomment-272015844
+jest.mock("./displayloader", () => mockEscapeHatch); //defualt export mock
+//******************************************************************************************** */
+
 jest.mock("./store", () => ({
   // named export mock
   dispatch: jest.fn()
