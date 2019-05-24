c3cb48b83035ba0d1deaedae3831c09428f4a57d
Bo Chen <bo.chen@westpac.com.au>
2019-05-24 11:45:47 +1000

    update

--- enzyme/jest.config.js
+++ enzyme/jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
     "bail": true,
     "testEnvironment": "jsdom",
     "setupFiles": ["<rootDir>/setup/shim.js", "<rootDir>/setup/index.js"],
-    // enable to snapshot shalloed result
+    // enable to snapshot shallowed result
     "snapshotSerializers": ["enzyme-to-json/serializer"]
 }
